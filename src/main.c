0a45fcde607fed417c492bab6141286d045b3a07
Priyan S <sprverse@gmail.com>
2026-03-13 02:55:37 -0400

    Implement menu-driven memory manager interface

--- src/main.c
+++ src/main.c
@@ -6,18 +6,52 @@ int main(){
     Block blocks[MAX_BLOCKS];
     int blockCount = 0;
 
-    initializeMemory(blocks, &blockCount);
-    displayMemory(blocks, blockCount);
-    allocateMemory(blocks, &blockCount, 30);
-    displayMemory(blocks, blockCount);
-    allocateMemory(blocks, &blockCount, 20);
-    displayMemory(blocks, blockCount);
-    removeMemory(blocks, blockCount, 30);
-    displayMemory(blocks, blockCount);
-    removeMemory(blocks, blockCount, 50);
-    displayMemory(blocks, blockCount);
-    mergeBlock(blocks, &blockCount);
-    displayMemory(blocks, blockCount);
+    int choice;
+    int start;
+    int size;
 
+    initializeMemory(blocks, &blockCount);
+
+    while(1) {
+        printf("\nMemory Manager\n");
+        printf("-------------------\n");
+        printf("1. Allocate Memory\n");
+        printf("2. Free Memory\n");
+        printf("3. Display Memory\n");
+        printf("4. Merge Free Blocks\n");
+        printf("5. Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+
+        switch(choice) {
+            case 1: 
+                printf("Enter memory size to allocate: ");
+                scanf("%d", &size);
+                allocateMemory(blocks, &blockCount, size);
+                break;
+            
+            case 2:
+                printf("Enter start address of block to free : ");
+                scanf("%d", &start);
+                removeMemory(blocks, blockCount, start);
+                break;
+
+            case 3:
+                displayMemory(blocks, blockCount);
+                break;
+
+            case 4:
+                mergeBlock(blocks, &blockCount);
+                printf("Free blocks merged./n");
+                break;
+
+            case 5:
+                printf("Exiting program...\n");
+                return 0;
+
+            default :
+                printf("invalid choice, Try again. \n");
+        }
+    }
     return 0;
 }
